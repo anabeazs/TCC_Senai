d5e62231a48fff7b13ef1e2245b5b4c1857f4496
Ana Beatriz <101596403+anabeazs@users.noreply.github.com>
2023-06-03 08:53:32 -0300

    Delete Home directory

--- Home/script.js
+++
@@ -1,8 +0,0 @@
-const ham =document.querySelector(".hamburger")
-const navBar = document.querySelector(".links")
-
-ham.addEventListener("click",()=>{
-    ham.classList.toggle("active")
-    navBar.classList.toggle("active")
-
-})
